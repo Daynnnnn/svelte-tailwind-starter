881a67805aa061604c6e49c9553c96cd14603bf2
Dan Pegg <daniel.pegg7849@gmail.com>
2021-01-07 10:04:16 +0000

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Tailwindcss from './Tailwind.svelte';
+
+	export let name;
+</script>
+<Tailwindcss />
+<main>
+	<div class="w-screen h-screen bg-gray-800">
+		<h1 class="text-white">Hello {name}!</h1>
+		<p class="text-white">Visit the <a class="text-blue-200 underline" href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</div>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./**/*.svelte",
+    "./**/*.html"
+ ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
